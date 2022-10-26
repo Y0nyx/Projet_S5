b7200905ff770ce0dc84ea02c36323a31401fb29
Y0nyx <veic1101@usherbrooke.ca>
2022-10-26 12:34:24 -0400

    Test acceleration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-import line_follower
-import Ultrasonic_Avoidance
+from SunFounder_Line_Follower import Line_Follower
+from SunFounder_Ultrasonic_Avoidance import Ultrasonic_Avoidance
 from picar import front_wheels
 from picar import back_wheels
 import picar
@@ -8,7 +8,7 @@ import time
 picar.setup()
 
 ua = Ultrasonic_Avoidance.Ultrasonic_Avoidance(20)
-lf = line_follower.Line_Follower()
+lf = Line_Follower.Line_Follower()
 fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 fw.turning_max = 45
@@ -23,63 +23,19 @@ timeout = 10
 last_angle = 90
 last_dir = 0
 
-def picar_setup():
-    bw.speed = forward_speed
 
-def opposite_angle():
-	global last_angle
-	if last_angle < 90:
-		angle = last_angle + 2* fw.turning_max
-	else:
-		angle = last_angle - 2* fw.turning_max
-	last_angle = angle
-	return angle
+def acceleration(final_speed, acceleration):
+    needed_time = final_speed/acceleration
+    bw.forward();
+    for i in range(0, final_speed):
+        bw.speed += 1
+        print(bw.speed)
+        time.sleep(needed_time/final_speed)
 
-def start_avoidance():
-    while True:
-        distance = ua.get_distance()
-        print("distance: %scm" % distance)
-
-        if distance > 0:
-            count = 0
-            if distance < back_distance:  # backward
-                print("backward")
-                fw.turn(opposite_angle())
-                bw.backward()
-                bw.speed = backward_speed
-                time.sleep(1)
-                fw.turn(opposite_angle())
-                bw.forward()
-                time.sleep(1)
-            elif distance < turn_distance:  # turn
-                print("turn")
-                # fw.turn(rand_dir())
-                bw.forward()
-                bw.speed = forward_speed
-                time.sleep(1)
-            else:
-                fw.turn_straight()
-                bw.forward()
-                bw.speed = forward_speed
-
-        else:  # forward
-            fw.turn_straight()
-            if count > timeout:  # timeout, stop;
-                bw.stop()
-            else:
-                bw.backward()
-                bw.speed = forward_speed
-                count += 1
 
 def stop():
 	bw.stop()
 	fw.turn_straight()
 
 if __name__ == '__main__':
-    picar_setup()
-	start_avoidance()
-    bw.forward()
-    print(lf.read_analog())
-    print(lf.read_digital())
-    print('')
-    time.sleep(0.5)
+    acceleration(70, 3)
