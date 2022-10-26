cbff32694cdf1c3676b49a4b63f5ffbdc50aa049
Y0nyx <veic1101@usherbrooke.ca>
2022-10-26 19:58:06 -0400

    v1.0.4

--- main.py
+++ main.py
@@ -7,26 +7,24 @@ import time
 
 picar.setup()
 
+_DEBUG = True
+_DEBUG_INFO = 'DEBUG INFO : '
+
 ua = Ultrasonic_Avoidance.Ultrasonic_Avoidance(20)
 lf = Line_Follower.Line_Follower()
 fw = front_wheels.Front_Wheels(db='config')
 bw = back_wheels.Back_Wheels(db='config')
 fw.turning_max = 45
 
-forward_speed = 30
-backward_speed = 70
+min_backward_speed = 30
+max_backward_speed = 50
 
-back_distance = 10
-turn_distance = 20
-
-timeout = 10
-last_angle = 90
-last_dir = 0
-
-
+def debug_(message):
+    if _DEBUG:
+        print(_DEBUG_INFO, message)
 def acceleration(final_speed, acceleration):
-    needed_time = final_speed/acceleration
-    bw.forward()
+    needed_time = (final_speed-min_backward_speed)/acceleration
+    bw.backward()
     for i in range(0, final_speed):
         bw.speed = i
         print(i)
@@ -36,37 +34,51 @@ def line_follower():
     while True:
         analog_data = lf.read_analog()
 
-        case = 0
+        case = -1
+        lower_value = 100
+        moy_analog_data = sum(analog_data)/len(analog_data)
+
         for i in range(len(analog_data) - 1):
-            if analog_data[i] < analog_data[i + 1]:
+            if analog_data[i] < lower_value and analog_data[i] < moy_analog_data*0.55:
+                lower_value = analog_data[i]
                 case = i
 
         if case == 0:
-            print("case 0")
-            fw.turn(30)
+            debug_("case 0")
+            last_angle = 15
+            fw.turn(last_angle)
         elif case == 1:
-            print("case 1")
-            fw.turn(5)
+            debug_("case 1")
+            last_angle = 65
+            fw.turn(last_angle)
         elif case == 2:
-            print("case 2")
+            debug_("case 2")
+            last_angle = 90
+            fw.turn(last_angle)
         elif case == 3:
-            print("case 3")
-            fw.turn(-5)
+            debug_("case 3")
+            last_angle = 115
+            fw.turn(last_angle)
         elif case == 4:
-            print("case 4")
-            fw.turn(-30)
-
-
-        time.sleep(0.5)
+            debug_("case 4")
+            last_angle = 145
+            fw.turn(last_angle)
+        elif case == -1:
+            debug_("LINE LOST")
+            last_angle = 90
+            fw.turn(last_angle)
+            bw.foward()
+            time(1)
+            bw.backward()
+            time(1)
+        time.sleep(0.1)
 def stop():
 	bw.stop()
 	fw.turn_straight()
 
 if __name__ == '__main__':
     try:
-        # acceleration(70, 3)
-        bw.speed = 10
-        bw.forward()
+        acceleration(max_backward_speed, 3)
         line_follower()
     except KeyboardInterrupt:
         stop()
